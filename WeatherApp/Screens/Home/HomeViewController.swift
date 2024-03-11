25c59bf4791fe83119978673593a075cec6c8c65
Waheed Afolabi <waheed.freelancer16@gmail.com>
2024-03-11 12:13:13 +0100

    Create a basic project's pages structure

--- WeatherApp/Screens/Home/HomeViewController.swift
+++ WeatherApp/Screens/Home/HomeViewController.swift
@@ -11,6 +11,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Home"
     }
 }
